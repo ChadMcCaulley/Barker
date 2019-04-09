a4fe611bf134227458f7be14a9200c55de158d4e
ChadMcCaulley <cha2168871@maricopa.edu>
2019-04-09 14:24:56 -0700

    Remove old folders as part of name change

--- warbler-server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {signin, signup} = require("../handlers/auth");   // Just bring in the signup function
-
-router.post("/signin", signin);
-router.post("/signup", signup);
-
-module.exports = router;
